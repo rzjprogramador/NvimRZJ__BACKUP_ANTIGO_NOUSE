4ad3e6acad31bf1c38d0160bd9fe7ef724975581
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-18 18:06:23 -0300

    feat: find com telescope

--- lua/maps/mapsPlugins.lua
+++ lua/maps/mapsPlugins.lua
@@ -1,13 +1,28 @@
 -- Maps PLUGINS --
+local set = api.nvim_set_keymap
+local ns = { noremap = true, silent = true }
+local cmd = vim.cmd
 
 -- explorer -> neo-tree
 
-vim.cmd([[ nnoremap <A-e> :NeoTreeFocusToggle<CR> ]])
-vim.cmd([[ inoremap <A-e> :NeoTreeFocusToggle<CR>l ]])
-vim.cmd([[ vnoremap <A-e> :NeoTreeFocusToggle<CR> ]])
+cmd([[ nnoremap <A-e> :NeoTreeFocusToggle<CR> ]])
+cmd([[ inoremap <A-e> :NeoTreeFocusToggle<CR>l ]])
+cmd([[ vnoremap <A-e> :NeoTreeFocusToggle<CR> ]])
 
 
--- Find -> Telescope -- TODO -- ESTA DANDO ERRO
--- vim.cmd([[ nnoremap <A-f> ::Telescope find_files<CR> ]])
--- vim.cmd([[ inoremap <A-f> ::Telescope find_files<CR> ]])
--- vim.cmd([[ vnoremap <A-f> ::Telescope find_files<CR> ]])
+-- Find -> Telescope -- TODO -- ESTA DANDO ERRO -
+set("n", "<A-f>", ":Telescope find_files<CR>", ns)
+set("i", "<A-f>", ":Telescope find_files<CR>", ns)
+set("v", "<A-f>", ":Telescope find_files<CR>", ns)
+
+-- Find Buffers -> Telescope --
+set("n", "<A-b>", ":Telescope buffers<CR>", ns)
+
+-- -- Find com telescope
+-- cmd([[ nnoremap <A-f> :Telescope find_files<CR> ]])
+-- cmd([[ inoremap <A-f> :Telescope find_files<CR> ]])
+-- cmd([[ vnoremap <A-f> :Telescope find_files<CR> ]])
+-- -- Find buffers com -> telescope
+-- cmd([[ nnoremap <A-b> :Telescope buffers<CR> ]])
+-- cmd([[ inoremap <A-b> :Telescope buffers<CR> ]])
+-- cmd([[ vnoremap <A-b> :Telescope buffers<CR> ]])
