2cca1caf6767e2cab3fb0b59ed8765a70ef12fe7
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-18 18:30:29 -0300

    feat: [def] exploer NeoTree Float

--- lua/maps/mapsPlugins.lua
+++ lua/maps/mapsPlugins.lua
@@ -1,12 +1,12 @@
 -- Maps PLUGINS --
 
--- explorer -> neo-tree
+-- EXPLORER
+-- explorer Float -> NeoTreeFloatToggle
+vim.cmd([[ nnoremap <A-e> :NeoTreeFloatToggle<CR> ]])
+vim.cmd([[ inoremap <A-e> :NeoTreeFloatToggle<CR> ]])
+vim.cmd([[ vnoremap <A-e> :NeoTreeFloatToggle<CR> ]])
 
-vim.cmd([[ nnoremap <A-e> :NeoTreeFocusToggle<CR> ]])
-vim.cmd([[ inoremap <A-e> :NeoTreeFocusToggle<CR>l ]])
-vim.cmd([[ vnoremap <A-e> :NeoTreeFocusToggle<CR> ]])
-
-
+-- FIND FLOAT
 -- Find -> Telescope -- TODO -- ESTA DANDO ERRO
 -- Find com telescope
 vim.cmd([[ nnoremap <A-f> :Telescope find_files<CR> ]])
@@ -16,5 +16,3 @@ vim.cmd([[ vnoremap <A-f> :Telescope find_files<CR> ]])
 vim.cmd([[ nnoremap <A-b> :Telescope buffers<CR> ]])
 vim.cmd([[ inoremap <A-b> :Telescope buffers<CR> ]])
 vim.cmd([[ vnoremap <A-b> :Telescope buffers<CR> ]])
--- vim.cmd([[ inoremap <A-f> ::Telescope find_files<CR> ]])
--- vim.cmd([[ vnoremap <A-f> ::Telescope find_files<CR> ]])
